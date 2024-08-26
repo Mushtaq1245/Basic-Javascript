14e85fad06c81530fb25d9e2e8432f4620447ce4
Muhamad Mushtaq <108513922+Mushtaq1245@users.noreply.github.com>
2024-08-26 11:33:41 +0000

    first topic Cover

--- learnjs/index.js
+++ learnjs/index.js
@@ -1,1 +1,7 @@
-console.log("Mushtaq")
+const accountId = 123456
+let accountName = "Muhammad Mushtaq"
+accountEmail = "mushaq@gmail.com"
+const accountPassword = 123
+
+console.table([accountId, accountName, accountEmail, accountPassword])
+
