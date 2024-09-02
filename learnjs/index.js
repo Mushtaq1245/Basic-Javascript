5b7f3c2c8e324940ba95e89757c921d1f3522936
Muhamad Mushtaq <108513922+Mushtaq1245@users.noreply.github.com>
2024-09-01 21:48:18 -0700

    Update index.js

--- learnjs/index.js
+++ learnjs/index.js
@@ -1,7 +1,18 @@
-const accountId = 123456
+// First Topic Let, Var, Const...
+
+/*const accountId = 123456
 let accountName = "Muhammad Mushtaq"
 accountEmail = "mushaq@gmail.com"
 const accountPassword = 123
 
 console.table([accountId, accountName, accountEmail, accountPassword])
+
+*/
+
+// Second Topic DATA TYPE
+
+let score = 33
+
+console.log(score)
+
 
