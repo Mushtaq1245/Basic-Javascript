0da640324cb64a4f8f45dbe55b95cb4519bd9650
Muhamad Mushtaq <108513922+Mushtaq1245@users.noreply.github.com>
2024-09-16 11:46:03 +0000

    files

---
+++ learnjs/string.js
@@ -0,0 +1,12 @@
+const name = "Muhammad Mushtaq"
+console.log(name);
+
+
+console.log(`My Name Is ${name}. My Father Name is Alamgir`);
+console.log("Muhammad Mushtaq");
+
+
+let gameName =  new string ("Mushtaq")
+
+console.log(gameName);
+
