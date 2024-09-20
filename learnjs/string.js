02c4d42598f1792595451ddf894fa3e95636135c
Muhamad Mushtaq <108513922+Mushtaq1245@users.noreply.github.com>
2024-09-20 05:04:15 +0000

    new type

--- learnjs/string.js
+++ learnjs/string.js
@@ -6,7 +6,15 @@ console.log(`My Name Is ${name}. My Father Name is Alamgir`);
 console.log("Muhammad Mushtaq");
 
 
-let gameName = new string('Mushtaq')
+const gameName = new String('Mushtaq')
 
-console.log(gameName.length);
+console.log(gameName)
+console.log(gameName.length)
+console.log(gameName.indexOf('t'))
+console.log(gameName.toUpperCase())
+
+const gameNameIS = 'Mushtaq'
+console.log(gameNameIS.slice-7, 4);
+
+
 
